fae504393eaa8576573d644b340cdb72625e69ed
Stefano Babini <stefano.babini5@studio.unibo.it>
2024-03-14 12:11:26 +0100

    finish part 2

--- src/main/scala/u02/Exercise02.scala
+++ src/main/scala/u02/Exercise02.scala
@@ -5,3 +5,20 @@ object Exercise02 extends App:
   def positive(x: Int): String = x match
     case x if x >= 0 => "positive"
     case _           => "negative"
+
+  val empty: String => Boolean = _ match
+    case "" => true
+    case _  => false
+
+  val neg: (String => Boolean) => (String => Boolean) = functionPredicate =>
+    (inputString => !functionPredicate(inputString))
+
+  val notEmpty: String => Boolean = neg(empty)
+
+  def negGeneric[A](predicate: A => Boolean): A => Boolean = input => !predicate(input)
+
+  val intPositive: Int => Boolean = _ match
+    case x if x >= 0 => true
+    case _           => false
+
+  val notPositive: Int => Boolean = negGeneric(intPositive)
--- src/test/scala/u02/Exercise02Test.scala
+++ src/test/scala/u02/Exercise02Test.scala
@@ -13,3 +13,15 @@ class Exercise02Test:
   @Test def testNegative =
     val a = positive(-1)
     assertEquals("negative", a)
+
+  @Test def testNotEmpty =
+    assertTrue(notEmpty("a"))
+
+  @Test def testEmpty =
+    assertFalse(notEmpty(""))
+  
+  @Test def testTrueGenericNeg = 
+    assertEquals(true, notPositive(-2))
+
+  @Test def testFalseGenericNeg = 
+    assertEquals(false, notPositive(0))
